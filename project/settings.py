fbf73b5651c17ee485556c302c6d62586dbded30
Abderahman <abdelrahman16174@gmail.com>
2025-09-03 10:24:31 +0300

    Fix DATABASES config for Railway PostgreSQL using dj-database-url

--- project/settings.py
+++ project/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 import dj_database_url
 DATABASES = {
     "default": dj_database_url.config(
